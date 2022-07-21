ec95fb57bb9bf8a5295a304cde80b7efe627737b
Simbelmyne <simbelmyne.evermind.131717@gmail.com>
2022-07-20 21:14:15 -0400

    fixed bug, add_product.html view section

--- ecommerce_multi_vendors/apps/vendor/views.py
+++ ecommerce_multi_vendors/apps/vendor/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.forms import UserCreationForm #for the form creation
 from django.shortcuts import render, redirect
 from .models import Vendor #import database
 from apps.product.models import Product #import db from the product app 
+from .forms import ProductForm 
+from django.utils.text import slugify
 
 def become_vendor(request):
     if request.method == 'POST': #check if the form has been submitted
@@ -30,3 +32,23 @@ def vendor_admin(request):
     products = vendor.products.all()
     
     return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products':products})
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            product = form.save(commit=False) #dont commit (false) until have his vendor 
+            product.vendor = request.user.vendor
+            product.slug = slugify(product.title)
+            product.save()
+            
+            return redirect('vendor_admin')
+        
+    else:
+        form = ProductForm()
+        
+    return render(request, 'vendor/add_product.html', {'form': form})        
+            
+        
