a57c024d5c8894f85193d56a4963a9ffd8e82108
Simbelmyne <simbelmyne.evermind.131717@gmail.com>
2022-07-28 20:41:48 -0400

    search function

--- ecommerce_multi_vendors/apps/product/views.py
+++ ecommerce_multi_vendors/apps/product/views.py
@@ -1,9 +1,13 @@
+from django.db.models import Q
 import random 
 from django.shortcuts import render, get_object_or_404
-
-
 from .models import Category, Product
 
+def search(request):
+    query = request.GET.get('query', '')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'product/search.html', {'products':products, 'query': query})
 
 def product(request, category_slug, product_slug):
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
